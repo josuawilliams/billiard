3a88c70e485e81fe9efb1bb0944b687b3cf6c421
Josua Williams <josuawilliams17@gmail.com>
2026-08-13 10:33:22 +0700

    feat: admin table CRUD with admin middleware

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TableController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -8,4 +9,12 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/tables', [TableController::class, 'index']);
+
+    Route::middleware('admin')->group(function () {
+        Route::post('/tables', [TableController::class, 'store']);
+        Route::put('/tables/{table}', [TableController::class, 'update']);
+        Route::delete('/tables/{table}', [TableController::class, 'destroy']);
+    });
 });
