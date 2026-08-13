21468db073b243807918a52c95d8766e6345d12d
Josua Williams <josuawilliams17@gmail.com>
2026-08-13 10:31:29 +0700

    feat: register login logout authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', function () {
-    return \App\Support\ApiResponse::success(['pong' => true], 'Pong');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
