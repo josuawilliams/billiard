a08b8d2519e2cf02b146537a78817c86aa5b666c
Josua Williams <josuawilliams17@gmail.com>
2026-08-15 16:41:15 +0700

    feat: add xendit payment config

--- config/payment.php
+++ config/payment.php
@@ -6,4 +6,9 @@ return [
     'mock' => [
         'server_key' => env('PAYMENT_MOCK_SERVER_KEY', 'mock-server-key'),
     ],
+
+    'xendit' => [
+        'api_key' => env('XENDIT_API_KEY'),
+        'callback_token' => env('XENDIT_CALLBACK_TOKEN'),
+    ],
 ];
